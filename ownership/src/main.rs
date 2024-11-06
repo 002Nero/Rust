88cdfbc1d051407ec3fd136847ce36dd58ff3db8
Valentin <valentinchastenet@gmail.com>
2024-11-06 14:08:20 +0100

    Types primitifs

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+
+    
+    let float1 = 5.15;
+    let float2 = 3.56;
+    let result = average(float1, float2); // c'est ici que les valeurs sont passees a la fonction
+    println!("La moyenne de {} et {} est {}", float1, float2, result);
+    let result = average(float1, float2);
+    println!("La moyenne de {} et {} est {}", float1, float2, result);
+
+
+
+
+}
+
+//Definir une fonction average() qui prends 2 flottants f64 en parametre et revoie leur moyenne
+fn average(premierfloat: f64, secondfloat: f64) -> f64 {
+    (premierfloat + secondfloat) / 2.0
 }
