27738f167aa165d9d2e66997bbadc3f27ed4bbf7
Valentin <valentinchastenet@gmail.com>
2024-11-06 14:27:30 +0100

    clonage / emprunt

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,19 +1,33 @@
 fn main() {
-
-    
     let float1 = 5.15;
     let float2 = 3.56;
-    let result = average(float1, float2); // c'est ici que les valeurs sont passees a la fonction
-    println!("La moyenne de {} et {} est {}", float1, float2, result);
-    let result = average(float1, float2);
-    println!("La moyenne de {} et {} est {}", float1, float2, result);
+    let result_average = average(float1, float2); // c'est ici que les valeurs sont passees a la fonction
+    println!("La moyenne de {} et {} est {}", float1, float2, result_average);
+    //---------------------------------------------------------------------------//
 
-
-
-
+    let rect = Rectangle {
+        length: 9.0,
+        width: 7.0,
+    };
+    
+    let result_perimeter = perimeter(rect.clone());
+    println!("Le périmètre du rectangle est: {}", result_perimeter);
 }
-
 //Definir une fonction average() qui prends 2 flottants f64 en parametre et revoie leur moyenne
 fn average(premierfloat: f64, secondfloat: f64) -> f64 {
     (premierfloat + secondfloat) / 2.0
 }
+
+//------------------------------------------------------------------------------//
+
+//Definissez la structure suivante
+#[derive(Clone)]
+struct Rectangle {
+    length : f64,
+    width : f64,
+}
+
+fn perimeter(rect: Rectangle) -> f64 {
+    2.0 * (rect.length + rect.width)
+}
+
