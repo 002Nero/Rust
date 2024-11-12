96f307d410c42cef89f98d63bb7633f8cc12af30
Valentin <valentinchastenet@gmail.com>
2024-11-12 14:02:12 +0100

    Iteration

--- options_and_results/src/bin/option.rs
+++ options_and_results/src/bin/option.rs
@@ -1,16 +1,21 @@
 fn main() {
     let string1 = "Bonjour Limoges";
     let string2 = "";
-    
+
     print_first_word1(string1);
     print_first_word1(string2);
+    
     print_first_word2(string1);
     print_first_word2(string2);
     
+    iterate_over_words(string1);
+    iterate_over_words(string2);
+    
+
 }
 
 //fonction qui prend en argument une ref vers une chaine et qui affiche son premier mot sous la forme suivante
-fn print_first_word1(string: &str) {    
+fn print_first_word1(string: &str) {
     // Diviser la chaîne en mots et récupérer le premier
     match string.split_whitespace().next() {
         Some(premier_mot) => println!("Premier mot: {}", premier_mot),
@@ -30,6 +35,17 @@ fn print_first_word2(string: &str) {
 
     println!("Premier mot: {}", premier_mot);
 }
+
+
+//fonction iterate_over_words() qui prend en argument une reference vers une chaine et qui affiche chacun de ses mots sur une ligne
+fn iterate_over_words(phrase: &str) {
+    // Diviser la chaîne en mots et itérer sur chacun d'eux
+    for mot in phrase.split_whitespace() {
+        println!("{}", mot);
+    }
+}
+
+
 
 
 
