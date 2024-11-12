fc1a9dcfc4e5e7e2a846012872e0135b3450a403
Valentin <valentinchastenet@gmail.com>
2024-11-12 14:27:47 +0100

    Exigence par .expect sur results.rs

--- options_and_results/src/bin/results.rs
+++ options_and_results/src/bin/results.rs
@@ -1,9 +1,12 @@
 fn main() {
     let string1 = "-17";
     let string2 = "Tux";
-    
+
     convert_to_int1(string1);
     convert_to_int1(string2);
+
+    convert_to_int2(string1);
+    convert_to_int2(string2);
 }
 
 fn convert_to_int1(chaine: &str) -> i32 {
@@ -18,5 +21,14 @@ fn convert_to_int1(chaine: &str) -> i32 {
             0
         }
     }
-    
+
 }
+
+//fonction convert_to_int2 qui prend en argument une reference vers une chaine et qui renvoie son entier et son carre ou panique s'il n'y en a pas avec .expect()
+fn convert_to_int2(chaine: &str) -> i32 {
+    // Convertir la chaîne en entier
+    let nombre = chaine.parse::<i32>().expect("La chaîne n'est pas un entier");
+    println!("Le carré de {} est {}", nombre, nombre * nombre);
+    nombre
+}
+
