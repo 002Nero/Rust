71e647dce31e225f7132ca6fa6c97ced90f86819
Valentin <valentinchastenet@gmail.com>
2024-11-12 13:49:57 +0100

    filtrage par match

--- options_and_results/src/bin/option.rs
+++ options_and_results/src/bin/option.rs
@@ -2,4 +2,19 @@ fn main() {
     let string1 = "Bonjour Limoges";
     let string2 = "";
     
+    print_first_word1(string1);
 }
+
+//fonction qui prend en argument une ref vers une chaine et qui affiche son premier mot sous la forme suivante
+fn print_first_word1(phrase: &str) {
+    // Diviser la chaîne en mots et récupérer le premier
+    match phrase.split_whitespace().next() {
+        Some(premier_mot) => println!("Premier mot: {}", premier_mot),
+        None => println!("La chaîne est vide ou ne contient pas de mots."),
+    }
+}
+
+
+
+
+
